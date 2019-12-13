29133b02d4910dd3beac0dbd0102da24d57cb4c5
Arisha Kashif <arishakashif05@gmail.com>
2019-12-13 21:13:18 +0500

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,119 @@
+home.php class="footer">
+
+<div class="row" style="margin: 0px;">
+
+<div id="loc" class="col-xs-12">
+
+<h1>  KARACHI . LAHORE .  JAPAN   </h1>
+
+</div>  
+
+</div>
+
+<br><br>
+
+<div style="margin-bottom: -30px">
+ 
+<div id="links" class="col-xs-12 col-md-4 col-lg-3">
+  
+<ol>
+  <br>
+  <li class="mainh"><span style="color:#043991;font-weight: bold">O</span>ur pages</li>
+  <hr>
+  <li><a href="home.php">Home</a></li>
+  
+  <li><a href="about.php" >About</a></li>
+    
+  <li><a href="allcar.php" >All Cars</a></li>
+
+  <li><a href="sell.php" >Sell your own car</a></li>
+  
+  <li><a href="contact.php" >Contact</a></li>
+  
+  <li><a href="review.php" >Review</a></li>
+    
+  <li class="lastlink"><a href="login.php" >My Account</a></li>
+    
+</ol>
+
+</div>
+
+<div id="hours" class="col-xs-12 col-md-4 col-lg-4" style="margin-left: -100px;">
+
+<ol>
+
+<li class="mainh"><span class="ourhour" >O</span>ur hours</li>
+<hr>
+<li >Monday
+<span >   10:00 - 19:00 </span>
+</li>
+<li>Tuesday
+<span>    10:00 - 19:00 </span>
+</li>
+<li>Wednesday
+<span>    10:00 - 19:00 </span>
+</li>
+<li>Thursday
+<span >   10:00 - 19:00 </span>
+</li>
+<li>Friday
+<span >   10:00 - 19:00 </span>
+</li>
+<li>Saturday
+<span >   12:00 - 19:00 </span>
+</li>
+<li class="isclosed">Sunday
+<span>    CLOSED  </span>
+</li>
+</ol>
+
+</div>
+
+<div id="add" class="col-xs-12 col-md-4 col-lg-4" style="margin-left: -100px">
+  
+<div class="address"><span style="color:#043991;font-weight: bold">O</span>ur Contact</div>
+<hr>  
+
+      <p><span  class="glyphicon glyphicon-map-marker"></span>  Nida Apartment , SB-5  Block-1  Gulshan-e-Iqbal Karachi,Pakistan</p>
+      <p><span  class="glyphicon glyphicon-phone"></span>  0333-3147737</p>
+      <p><span  class="glyphicon glyphicon-envelope" ></span>  umairkhan@gmail.com</p>
+  
+
+</div>
+
+</div>
+
+<br>
+
+
+  
+<!-- Add font awesome icons -->
+
+<div  >
+
+<div class="row" style="margin-top: -30px;">
+
+<div class="col-lg-12" id="follow">
+<p> Follow us</p>
+<hr></div>
+
+</div>
+<div class="row" >
+
+<div class="col-lg-12" id="followlinks" style="margin: 0px;">
+
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-snapchat"></a>
+<a href="#" class="fa fa-instagram"></a>
+<a href="#" class="fa fa-google"></a>
+</div>
+
+
+</div>
+
+<br>
+
+
+</div>
+
